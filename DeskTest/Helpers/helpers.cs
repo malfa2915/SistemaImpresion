e56e2aba035ea8ba75566ed99a5811dcbf745692
GaRIEN <gabrielhuaman683@gmail.com>
2025-07-14 17:06:04 -0500

    avance

--- DeskTest/Helpers/helpers.cs
+++ DeskTest/Helpers/helpers.cs
@@ -8,12 +8,12 @@ public static class helpers
 {
 
     //public static string url = "http://25.1.115.6:8082/api/";
-    public static string url = "https://localhost:44357/";
+    //public static string url = "https://localhost:44357/";
     //public static string url = "https://10454403547donayreapi.spk.com.pe/";
 
     //public static string url = "http://192.168.1.50:8095/";
     //public static string url = "https://10454403547donayreapi.spk.com.pe/";
-    //public static string url = "https://spkdemoapi.spk.com.pe/";
+    public static string url = "https://spkdemoapi.spk.com.pe/";
 
 
 }
