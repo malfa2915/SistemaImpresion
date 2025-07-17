903efd3ca593156e9dc955e0e29d1b9bf0c2910e
fabio miranda <radic_fmj@hotmail.com>
2025-07-17 17:44:56 -0500

    cambios17072025

--- DeskTest/Api/ImpresorasNegocioAPI.cs
+++ DeskTest/Api/ImpresorasNegocioAPI.cs
@@ -249,6 +249,31 @@ public class ImpresorasNegocioAPI
         }
     }
 
+
+    public static async Task<List<detalleItemsImpresoras>> getListImpresorasXIdImpresora(List<int> print)
+    {
+        var httpClient = new HttpClient();
+        var json = JsonConvert.SerializeObject(print);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await httpClient.PostAsync(helpers.url + "api/ImpresoraNegocio/getListImpresorasXIdImpresora", content);
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<detalleItemsImpresoras>>(jsonResult);
+            return result;
+        }
+        else
+        {
+            var responseError = new HttpResponseMessage(HttpStatusCode.NotFound)
+            {
+                Content = new StringContent(string.Format("Verificar los campos")),
+                ReasonPhrase = "Verificar los compos"
+            };
+
+            return null;// throw new HttpResponseException(responseError);
+        }
+    }
+
     public static async Task<ImpresorasNegocio> GetINServicio(Boolean print,string empresa, int IdCentro)
     {
         var httpClient = new HttpClient();
